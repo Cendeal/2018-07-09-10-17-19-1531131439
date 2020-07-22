aeb3e05b7c5ae6edb2aed5f3a50903c112441879
Cendeal <5205093You>
2020-07-22 17:59:06 +0800

    [practice07][@cendeal]feature:finished practice 07, add Klass and complete all Class,  test passed.

--- src/main/java/practice07/Teacher.java
+++ src/main/java/practice07/Teacher.java
@@ -1,4 +1,39 @@
 package practice07;
 
-public class Teacher {
+
+public class Teacher extends Person {
+    private Klass klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Klass klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    public String introduceWith(Student student) {
+        boolean isTechTheStudent = this.isTeach(student.getKlass());
+        return super.introduce() + String.format(" I%s teach %s.", isTechTheStudent ? "" : " don't", student.getName());
+    }
+
+    private boolean isTeach(Klass klass) {
+        return this.getKlass().getNumber() == klass.getNumber();
+    }
+
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Teacher."
+                + (this.klass == null ? " I teach No Class." : String.format(" I teach %s.", this.klass.getDisplayName()));
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
 }
