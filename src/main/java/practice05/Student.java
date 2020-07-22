f7bb22f4b908cb8f30453478136e7af8484294b8
Cendeal <5205093You>
2020-07-22 15:53:48 +0800

    [practice05][@cendeal]feature:complete Person class feature,Student class feature and  Worker class feature, test passed.

--- src/main/java/practice05/Student.java
+++ src/main/java/practice05/Student.java
@@ -1,4 +1,31 @@
 package practice05;
 
-public class Student {
+import practice02.Person;
+
+public class Student extends Person {
+    private int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old. I am a Student. I am at Class %d.", this.getName(),this.getAge(),this.klass);
+    }
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
 }
+
+
