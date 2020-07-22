c4517f335674b9cb5666dd35c1a89c7fa24e5154
Cendeal <5205093You>
2020-07-22 16:08:54 +0800

    [practice06][@cendeal]feature:complete Person,Student,  Worker class and Teacher class feature,teacher introduce witch his/her klass info, test passed.

--- src/main/java/practice06/Teacher.java
+++ src/main/java/practice06/Teacher.java
@@ -1,4 +1,28 @@
 package practice06;
 
-public class Teacher {
+public class Teacher extends Person {
+    private Integer klass;
+
+    public Teacher(String name, int age) {
+        super(name, age);
+    }
+
+    public Teacher(String name, int age, Integer klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old. I am a Teacher.", this.getName(), this.getAge())
+                + (this.klass == null ? " I teach No Class." : String.format(" I teach Class %d.", this.klass));
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
 }
