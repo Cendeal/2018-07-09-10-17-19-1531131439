fcfb89e24d6ef1b86b60fadf67c162d62078f9e9
Cendeal <5205093You>
2020-07-22 19:39:41 +0800

    [practice09][@cendeal]feature:rewrite appendMember and let its field as map,finished Person,Teacher,Student and Klass. test passed.

--- src/main/java/practice09/Klass.java
+++ src/main/java/practice09/Klass.java
@@ -1,4 +1,50 @@
 package practice09;
 
+
+import java.util.HashSet;
+import java.util.Set;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private final Set<Student> members;
+
+
+    public Klass(int number) {
+        this.members = new HashSet<>();
+        this.number = number;
+    }
+
+    public void appendMember(Student student) {
+        this.members.add(student);
+    }
+
+    public boolean isLeader(Student student) {
+        return this.leader != null && this.leader.getId() == student.getId();
+    }
+
+    public void assignLeader(Student student) {
+        if (this.members.contains(student)) {
+
+            this.leader = student;
+            return;
+        }
+        System.out.print("It is not one of us.\n");
+    }
+
+    public Student getLeader() {
+        return this.leader;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %d", this.getNumber());
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
--- src/main/java/practice09/Teacher.java
+++ src/main/java/practice09/Teacher.java
@@ -1,4 +1,39 @@
 package practice09;
 
-public class Teacher {
+public class Teacher extends Person{
+    private Klass klass;
+
+    public Teacher(int id, String name, int age) {
+        super(id, name, age);
+    }
+
+    public Teacher(int id, String name, int age, Klass klass) {
+        super(id, name, age);
+        this.klass = klass;
+    }
+
+
+    private boolean isTeach(Klass klass) {
+        return this.getKlass().getNumber() == klass.getNumber();
+    }
+
+    @Override
+    public String introduce() {
+        return super.introduce() + " I am a Teacher."
+                + (this.klass == null ? " I teach No Class." : String.format(" I teach %s.", this.klass.getDisplayName()));
+    }
+
+    public String introduceWith(Student student) {
+        boolean isTechTheStudent = this.isTeach(student.getKlass());
+        return super.introduce() + String.format(" I am a Teacher. I%s teach %s.",
+                isTechTheStudent ? "" : " don't", student.getName());
+    }
+
+    public Klass getKlass() {
+        return klass;
+    }
+
+    public void setKlass(Klass klass) {
+        this.klass = klass;
+    }
 }
