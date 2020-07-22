3e72d1e71bb0dffc20aebc922567e3c50b21ba5e
Cendeal <5205093You>
2020-07-22 18:55:31 +0800

    [practice08][@cendeal]feature:rewrite equal method and hash method for perso,  test passed.

--- src/main/java/practice08/Klass.java
+++ src/main/java/practice08/Klass.java
@@ -1,4 +1,33 @@
 package practice08;
 
 public class Klass {
+    private int number;
+    private Student leader;
+
+    public Klass(int number) {
+        this.number = number;
+    }
+
+    public boolean isLeader(Student student){
+        return this.leader != null && this.leader.getId() == student.getId();
+    }
+
+    public void assignLeader(Student student){
+        this.leader = student;
+    }
+
+    public Student getLeader(){
+        return this.leader;
+    }
+    public String getDisplayName(){
+        return String.format("Class %d",this.getNumber());
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
