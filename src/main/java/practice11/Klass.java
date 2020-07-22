10f7be3aa9ba478b7f6cbe7c8fdd8e52d03fbbd5
Cendeal <5205093You>
2020-07-22 22:29:22 +0800

    [practice11][@cendeal]feature:finish some class define.

--- src/main/java/practice11/Klass.java
+++ src/main/java/practice11/Klass.java
@@ -1,4 +1,58 @@
 package practice11;
 
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.Set;
+import java.util.stream.Collectors;
+
 public class Klass {
+    private int number;
+    private Student leader;
+    private final Set<Student> members;
+    private Collection<JoinListener> joinListeners;
+
+
+    public Klass(int number) {
+        this.members = new HashSet<>();
+        this.number = number;
+        joinListeners = new ArrayList<>();
+    }
+
+    public void appendMember(Student student) {
+        this.members.add(student);
+    }
+
+    public boolean isIn(Student student) {
+        return this.members.contains(student);
+    }
+
+    public boolean isLeader(Student student) {
+        return this.leader != null && this.leader.getId() == student.getId();
+    }
+
+    public void assignLeader(Student student) {
+        if (this.members.contains(student)) {
+            this.leader = student;
+            return;
+        }
+        System.out.print("It is not one of us.\n");
+    }
+
+    public Student getLeader() {
+        return this.leader;
+    }
+
+    public String getDisplayName() {
+        return String.format("Class %d", this.getNumber());
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public void setNumber(int number) {
+        this.number = number;
+    }
 }
