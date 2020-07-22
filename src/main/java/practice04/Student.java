af13346b68c6168b2d195b09f7e29c0fa95ef9c6
Cendeal <5205093You>
2020-07-22 15:46:13 +0800

    [practice04][@cendeal]feature:complete Person class feature, override Student introduce feature and override introduce for Worker class, test passed.

--- src/main/java/practice04/Student.java
+++ src/main/java/practice04/Student.java
@@ -1,4 +1,30 @@
 package practice04;
 
-public class Student {
+import practice02.Person;
+
+public class Student extends Person {
+    private int klass;
+
+    public Student(String name, int age, int klass) {
+        super(name, age);
+        this.klass = klass;
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old. I am a Student. I am at Class %d.", this.getName(),this.getAge(),this.klass);
+    }
+
+    public Student(String name, int age) {
+        super(name, age);
+    }
+
+    public int getKlass() {
+        return klass;
+    }
+
+    public void setKlass(int klass) {
+        this.klass = klass;
+    }
 }
+
--- src/main/java/practice04/Worker.java
+++ src/main/java/practice04/Worker.java
@@ -1,4 +1,14 @@
 package practice04;
 
-public class Worker {
+import practice03.Person;
+
+public class Worker extends Person {
+    public Worker(String name, int age) {
+        super(name, age);
+    }
+
+    @Override
+    public String introduce() {
+        return String.format("My name is %s. I am %d years old. I am a Worker. I have a job.",this.getName(),this.getAge());
+    }
 }
