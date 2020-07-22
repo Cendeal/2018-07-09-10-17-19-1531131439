1aec2b8d6047bbdb8d80aa80bd1ff73dc4dfca59
Cendeal <5205093You>
2020-07-22 16:41:39 +0800

    [practice05][@cendeal]fixed bug:fixed the requirement of practice04 about introduce function, test passed.

--- src/main/java/practice05/Worker.java
+++ src/main/java/practice05/Worker.java
@@ -9,6 +9,6 @@ public class Worker extends Person {
 
     @Override
     public String introduce() {
-        return String.format("My name is %s. I am %d years old. I am a Worker. I have a job.",this.getName(),this.getAge());
+        return super.introduce()+" I am a Worker. I have a job.";
     }
 }
